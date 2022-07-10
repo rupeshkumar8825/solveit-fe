fba451eebde19e9b618091a4f35408984fd6c420
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-07-10 22:26:58 +0530

    Made the basic navbar. Moving to create the routes for the solveit pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Navigation from "./components/Navigation";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap/dist/react-bootstrap.min.js";
+
 
 function App() {
   return (
     <>
+      <Navigation></Navigation>
       <h1>hi this is home page of solveit </h1>
     </>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,54 @@
+// in this i will make the navigation bar for solve it 
+import React from "react";
+import { Navbar, Nav,  Container, Button, Form, FormControl} from "react-bootstrap";
+// import { MDBCol, MDBIcon } from "mdbreact";
+import 'bootstrap/dist/css/bootstrap.css';
+// import { Link } from 'react-router-dom';
+
+// defining the functional component for this purpose 
+const  Navigation = ()=>{
+    console.log("this is navigation menu ");
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand className="mr-10" href="#home">Solveit</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse  id="responsive-navbar-nav">
+                    {/* <Nav className="me-10" > */}
+                    <Nav className="me-auto" >
+                    <Nav.Link className = "ml-5 " href="#features">Home</Nav.Link>
+                    <Nav.Link className = "ml-5 "  href="#pricing">Trending Problems</Nav.Link>
+                    <Nav.Link className = "ml-5 " href="#pricing">Unicorn Tracker</Nav.Link>
+                    <Nav.Link className = "ml-5 " href="#pricing">Upload Problem</Nav.Link>
+                    <Nav.Link className = "ml-5 " href="#pricing">Signin/Register</Nav.Link>
+                    
+                    </Nav>
+
+                    <Nav>
+                        
+                        <Form className="d-flex">
+                            <FormControl
+                             size="sm"
+                            type="search"
+                            placeholder="Search by Category"
+                            className="me-2"
+                            aria-label="Search"
+                            />
+                            <Button  size="sm" variant="danger">Search</Button>
+                        </Form>
+                        {/* <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link> */}
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+// exporting this navbar to be able to use the other components 
+export default  Navigation;
