00ecb199857667babe9785206ae1d52945dbce86
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-07-27 17:31:44 +0530

    showed the details of the idea. Moving to implement the option to like, share and comment as well for this purpose

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ import Register from "./pages/Register";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { useEffect } from "react";
+import { useState } from "react";
+import { ideasAction } from "./redux/action/ideasAction";
+import { postsDetailsAction } from "./redux/action/postsDetailsAction";
+import { usersAction } from "./redux/action/usersAction";
+
+// import { useDispatch } from "react-redux";
+
 // import browserHistory
 // import { browserHistory } from 'react-router';
 
@@ -26,8 +33,11 @@ import DetailedPost from "./pages/DetailedPost";
 function App() {
 	// const navigate =  useNavigate();
 	// const isLoggedin = useSelector((state)=> state.isLoggedInReducer);
-    const isLoggedIn = useSelector((state) => state.IsLoggedInReducer.isLoggedIn)
-	console.log(isLoggedIn)
+    // const isLoggedIn = useSelector((state) => state.IsLoggedInReducer.isLoggedIn)
+	// console.log(isLoggedIn)
+    const [posts, setposts] = useState([]);
+	const dispatch = useDispatch();
+
 	const username = useSelector((state)=> state.userNameReducer.username);
 	const getListOfIdeas = async ()=>{
         const URL = "http://127.0.0.1:8000/ideas";
@@ -41,24 +51,137 @@ function App() {
 		console.log(response.data)
 		
     }
-	
+	const fetchDetails = async()=>{
+		const url = "http://127.0.0.1:8000/";
+        const headers = {
+            'Content-Type' : 'application/json',
+            "Access-Control-Allow-Origin" : "*",
+            "withCredentials": true
+            
+        }
+        
+		const response = await axios.get(url, headers);
+		const ideas = response.data.ideas;
+        const users = response.data.users;
+        // STORING THE LIST OF IDEAS AND USERS  
+        dispatch(ideasAction(ideas));
+        dispatch(usersAction(users));
+		
+		
+        let imgList = [];
+
+        ideas.forEach(element => {
+			imgList.push(element.thumbnail);
+        })
+		
+		
+        
+        const postsListDetails = [];
+		
+        // USING THE FOR LOOP TO STORE ALL THE IMAGES HERE FOR THIS PURPOSE 
+        imgList.forEach(async element => {
+			// WE HAVE TO MAKE  A GET REQUEST TO BACKEND TO BRING THE IMAGE FOR THIS PURPOSE AND STORE IT IN THE ARRAY 
+            const options = {
+                'responseType': 'blob',
+                "Access-Control-Allow-Origin" : "*",
+                "withCredentials": true
+            }
+			
+			
+            const response2 = await axios.get(`http://127.0.0.1:8000/image?path=${element}`, options);
+            const imageObjectURL = URL.createObjectURL(response2.data);
+            
+			
+			
+            // AGAIN WE HAVE TO FIND THE ID OF IDEA CORRESPONDING TO THIS PICTURE 
+            // let currId = null;
+            let curruserId = null;
+            let currIdeaId = null;
+            let currEmail = null;
+            let currfirstname = null;
+            let currlastname = null;
+            let currphone = null;
+            let currusername = null;
+            let currcategory = null;
+            let currdescription = null;
+            let currideaname = null;
+            let currothersknow = null;
+            let currrating = null;
+			
+            // let 
+            ideas.forEach(element2 => {
+				if(element2.thumbnail === element)
+                {
+					currIdeaId = element2._id;
+                    curruserId = element2.user_id;
+                    currcategory = element2.category;
+                    currdescription = element2.description;
+                    currideaname = element2.ideaname;
+                    currothersknow = element2.othersknow;
+                    currrating = element2.rating;
+                }
+            });
+
+            // NOW I HAVE TO SEARCH THE USER CORRESPONDING TO THAT USER ID 
+            users.forEach(element2 => {
+				if(element2._id === curruserId)
+                {
+                    currEmail = element2.email;
+                    currfirstname = element2.firstname;
+                    currlastname = element2.lastname;
+                    currphone = element2.phone;
+                    currusername = element2.username;
+                }
+            });
+            const tempObj = {
+				userId : curruserId,
+                ideaId : currIdeaId,
+                email : currEmail,
+                firstname : currfirstname,
+                lastname : currlastname,
+                phone : currphone,
+                username : currusername,
+                url : imageObjectURL,
+                category : currcategory,
+                description : currdescription,
+                ideaname : currideaname,
+                othersknow : currothersknow,
+                rating : currrating,
+				
+				
+            };
+            
+            
+            postsListDetails.push(tempObj);
+            setposts(postsListDetails);
+            
+			
+        });
+        
+        console.log("The list of urls of images that we got is as follows\n");
+        console.log(postsListDetails);
+        dispatch(postsDetailsAction(postsListDetails));
+        console.log("The response from the backend to the home page is as follows\n");
+        console.log(response.data);
+	}
+		
 	useEffect(() => {
 		// getListOfIdeas();
-		
+		fetchDetails();	
 	}, []);
-	// useEffect(async () => {
-	// 	// WE HAVE TO MAKE THE AXIOS POST REQUEST 
-	// 	const URL = "http://localhost:8080/";
-    //     const headers = {
-    //         'Content-Type' : 'application/json',
+		// useEffect(async () => {
+			// 	// WE HAVE TO MAKE THE AXIOS POST REQUEST 
+			// 	const URL = "http://localhost:8080/";
+			//     const headers = {
+				//         'Content-Type' : 'application/json',
     //         "Access-Control-Allow-Origin" : "*"
     //     }
 	// 	const response = await axios.post(URL, headers);
 	// 	console.log(response)
 	// 	if(response.data.status == 200)
 	// 	{
-	// 		// THIS MEANS THAT USER IS ALREADY LOGGED IN 
-	// 		console.log("The user is already loggedin\n");
+		// 		// THIS MEANS THAT USER IS ALREADY LOGGED IN 
+		// 		console.log("The user is already loggedin\n");
 	// 	}
 	// 	else if(response.data.status == 401)
 	// 	{
